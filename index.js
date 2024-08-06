7f7692dc5448b9cdb8037425e90dcec62389e547
itsalaassar <itsalaassar@gmail.com>
2024-08-06 03:56:30 +0300

    fix connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import { DBConnection } from './DB/Connection.js';
 const app = express();
 
+app.use(express.json()) 
+DBConnection()
 app.listen(3000,()=>{
     console.log("----------------Server is running on port 3000----------------------");
 })
